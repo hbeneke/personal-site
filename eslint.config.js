082518eb153f3e6ac411ecbe7b50793fcac9f8b0
Habakuk Beneke <habakukbeneke@proton.me>
2024-11-25 10:40:10 +0100

    Clean up the project to start working on features

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    env: {
+      node: true,
+    },
+  },
   pluginJs.configs.recommended,
 ];

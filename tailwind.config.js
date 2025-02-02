f75f903d0bba61e33d8aa59fe476962a121d95f8
Habakuk Beneke <habakukbeneke@proton.me>
2025-02-02 19:41:01 +0100

    feature: Add jetbrains font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,16 @@ export default {
   content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
   theme: {
     extend: {
-      colors: {
-        backgroundDark: "#1e1e1e",
-        backgroundLight: "#f9f9f9",
-        textLight: "#ffffff",
-        textDark: "#1e1e1e",
-
-        accentBlue: "#4FC1FF",
-        accentPurple: "#C586C0",
-        accentGreen: "#B5CEA8",
-
-        grayDark: "#2D2D2D",
-        grayLight: "#d4d4d4",
+      fontFamily: {
+        jetbrains: [
+          "JetBrains Mono",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          "sans-serif",
+        ],
       },
     },
   },

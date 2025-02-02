bc671d2bd6bc3c4c1caca41e071743e427efcb31
Habakuk Beneke <habakukbeneke@proton.me>
2025-02-02 19:09:08 +0100

    Increase libraries version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundDark: "#1e1e1e",
+        backgroundLight: "#f9f9f9",
+        textLight: "#ffffff",
+        textDark: "#1e1e1e",
+
+        accentBlue: "#4FC1FF",
+        accentPurple: "#C586C0",
+        accentGreen: "#B5CEA8",
+
+        grayDark: "#2D2D2D",
+        grayLight: "#d4d4d4",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };

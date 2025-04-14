e49f9f1540bd07b9399a546332f243aba1c586b6
Habakuk Beneke <habakukbeneke@proton.me>
2025-04-14 14:31:43 +0200

    feat: add new commands for playwright test
    Added new command-line options to improve test automation workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,29 +1,37 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./tests/e2e",
-  fullyParallel: true,
-  workers: 5,
-  retries: 2,
-  reporter: "html",
-  use: {
-    trace: "on-first-retry",
-    baseURL: "http://localhost:4321",
-  },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
+	testDir: "./tests/e2e",
+	fullyParallel: true,
+	workers: 5,
+	retries: 2,
+	reporter: "html",
+	use: {
+		trace: "on-first-retry",
+		baseURL: "http://localhost:4321",
+	},
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+	webServer: {
+		command: "npm run start",
+		url: "http://localhost:4321",
+		reuseExistingServer: !process.env.CI,
+		timeout: 120 * 1000,
+	},
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
+
+		{
+			name: "webkit",
+			use: { ...devices["Desktop Safari"] },
+		},
+	],
 });

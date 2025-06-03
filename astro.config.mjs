19371643c9e42d399124cd94113f547058a94aed
Habakuk Beneke <habakukbeneke@proton.me>
2025-06-03 14:56:40 +0200

    feat: add Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [tailwind()],
+  adapter: vercel(),
 });

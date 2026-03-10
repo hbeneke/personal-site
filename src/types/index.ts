876b2828abb62ee06c2ab73000b478cbf18fc910
Habakuk Beneke <habakukbeneke@proton.me>
2026-03-10 12:20:10 +0100

    refactor(types): remove dead types from pre-Content Collections era

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,6 @@ export * from "@/types/menu-link.type";
 export * from "@/types/work-experience.type";
 export * from "@/types/note.type";
 export * from "@/types/project.type";
-export * from "@/types/post.type";
 export * from "@/types/rss.type";
 export * from "@/types/tag.type";
 export * from "@/types/skill.type";
--- src/types/note.type.ts
+++ src/types/note.type.ts
@@ -6,10 +6,4 @@ export interface Note {
   starred: boolean;
 }
 
-export interface NotesCollection {
-  data: {
-    notes: Note[];
-  };
-}
-
 export interface GroupedNotesByYear extends Array<[string, Note[]]> {}
